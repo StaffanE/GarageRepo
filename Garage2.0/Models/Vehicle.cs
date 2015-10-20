6f349b19dc557d4fd68cf8566dc7036e10bf583f
mattza <matti.blandat@gmail.com>
2015-10-20 16:55:21 +0200

    Created Datamodel, Vehicle Class, DataAccessContext

---
+++ Garage2.0/Models/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2._0.Models
+{
+    public class Vehicle
+    {
+        public string VehicleType { get; set; }
+        public string RegNumber { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Make { get; set; }
+        public int NumberOfWheels { get; set; }
+
+    }
+}
