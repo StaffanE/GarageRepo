29ef106b52d983ec8cb04de8ade8a7689cefefeb
JonasUE <jonasjakobsson.sundbyberg@gmail.com>
2015-10-21 13:21:31 +0200

    Set DisplayNames in vehicle model

--- Garage2.0/Models/Vehicle.cs
+++ Garage2.0/Models/Vehicle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garage2._0.Models
 {
@@ -9,13 +10,21 @@ namespace Garage2._0.Models
 
     public class Vehicle
     {
+        
         public int Id { get; set; }
-        public VehicleEnum VehicleType { get; set; }
+        [Display(Name = "Fordonstyp")]
+        public VehicleEnum VehicleType  { get; set; }
+        [Display(Name = "Reg. nummer")]
         public string RegNumber { get; set; }
+        [Display(Name = "Färg")]
         public string Color { get; set; }
+        [Display(Name = "Fabrikat")]
         public string Brand { get; set; }
+        [Display(Name = "Modell")]
         public string Make { get; set; }
+        [Display(Name = "Antal hjul")]
         public int NumberOfWheels { get; set; }
+        [Display(Name = "Parkerad (ankomsttid)")]
         public DateTime ArrivalTime { get; set; }
 
 
